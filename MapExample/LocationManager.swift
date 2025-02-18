df491e4e9d9a356c96d09f287f2d1b1f33f4a339
sarahdadoun01 <85398444+sarahdadoun@users.noreply.github.com>
2025-02-18 12:59:53 -0500

    added project files

---
+++ MapExample/LocationManager.swift
@@ -0,0 +1,20 @@
+//
+//  LocationManager.swift
+//  MapExample
+//
+//  Created by Sarah Dadoun on 2025-02-18.
+//
+
+import Foundation
+import MapKit
+import CoreLocation
+
+
+class LocationManager : NSObject, ObservableObject, CLLocationManagerDelegate {
+    
+    // initialize location manager
+    private let locationManager = CLLocationManager()
+    @Published
+    
+    // init
+}
---
+++ MapExample/MapExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapExampleApp.swift
+//  MapExample
+//
+//  Created by Sarah Dadoun on 2025-02-18.
+//
+
+import SwiftUI
+
+@main
+struct MapExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
