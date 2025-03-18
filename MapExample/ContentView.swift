8c8da2cce9d30ba68cdb8f51e86ae4c0a972feed
sarahdadoun01 <85398444+sarahdadoun@users.noreply.github.com>
2025-03-18 12:28:47 -0400

    made map functional

--- MapExample/ContentView.swift
+++ MapExample/ContentView.swift
@@ -6,18 +6,39 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 45.494183, longitude: -73.581283),
+        span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+    )
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        Map(coordinateRegion: $region, annotationItems: [Landmark(name: "College LaSalle", coordinate: region.center)]) { landmark in
+            MapAnnotation(coordinate: landmark.coordinate) {
+                VStack {
+                    Image(systemName: "mappin.circle.fill")
+                        .resizable()
+                        .frame(width: 30, height: 30)
+                        .foregroundColor(.red)
+                    Text(landmark.name)
+                        .font(.caption)
+                        .background(Color.white.opacity(0.7))
+                        .cornerRadius(5)
+                }
+            }
+        }.edgesIgnoringSafeArea(.all)
     }
 }
+
+struct Landmark: Identifiable {
+    let id = UUID()
+    let name: String
+    let coordinate: CLLocationCoordinate2D
+}
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
--- MapExample/LocationManager.swift
+++ MapExample/LocationManager.swift
@@ -14,7 +14,22 @@ class LocationManager : NSObject, ObservableObject, CLLocationManagerDelegate {
     
     // initialize location manager
     private let locationManager = CLLocationManager()
-    @Published
+    @Published  var userLocation: CLLocationCoordinate2D? // permission -> yes
     
     // init
+    override init(){
+        super.init() // will fetch the previous init code, and add the new ones we write.
+        
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingHeading() // observer to mapkit
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]){
+        guard let latestLocation = locations.last else {return}
+        DispatchQueue.main.async {
+            self.userLocation = latestLocation.coordinate
+        }
+    }
+    // user location
 }
